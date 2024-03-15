8c4a06f9736cf7fadafe923ffb0ae37d064f4fc7
최지훈 <hunnx27@naver.com>
2024-03-15 17:10:54 +0900

    add: 신규포스팅 로직 준비(TODO)

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ for post in postlist:
     # 변수 초기화
     title = post['title']
     link = post['link']
-
     # 레디스 변수 넣고 처리(true인 경우 신규, false는 기처리라 패쓰)
     if rlib.scrape_and_store_data(title, link):
-        print('신규처리 : {} | {}'.format(title, link))
+        try:
+            print('신규 포스팅 시작 : {} | {}'.format(title, link))
+            
+        except Exception as e:
+            print(e)
+            rlib.removeKey(link) # 신규등록 과정에서 오류 시 레디스에서 키를 제거해서 다시 수행할 수 있게 함
 
     # TODO 자동화 포스팅 로직 넣어야함
         print('TODO 티스토리 자동화 포스팅 작업중')
